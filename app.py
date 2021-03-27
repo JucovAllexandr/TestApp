2d9faa2642974325f67f53211d5e09b6f3b4f349
Jucov Alexandr <sania337@gmail.com>
2021-03-27 13:13:43 +0200

    cm 3

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def returnHome():
+    return "Hello world"
+
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
