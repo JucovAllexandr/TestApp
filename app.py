c2dc86d7df6a69bae681913891c1b6c697429f34
JucovAlexandr <sania337@gmail.com>
2021-03-27 17:35:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def returnHome():
-    return "Hello world"
+    return "Hello world!"
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
